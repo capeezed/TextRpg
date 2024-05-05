4ab9c95faa4dc586520b6b7f41a376eb05909101
capeezed <capeelini@gmail.com>
2024-05-05 16:41:04 -0300

    WIP

---
+++ RpgText/src/Files/Enemy.java
@@ -0,0 +1,20 @@
+package Files;
+
+public class Enemy {
+
+	public String name;
+	public int health;
+	public int attack;
+	
+	public Enemy(String name, int health, int attack) {
+		
+		this.name = name;
+		this.health = health;
+		this.attack = attack;
+		
+	}
+	
+	
+	
+	
+}
---
+++ RpgText/src/Files/Player.java
@@ -0,0 +1,19 @@
+package Files;
+
+public class Player {
+
+	public String name;
+	public int health;
+	public int attack;
+	
+	public Player(String name, int health, int attack) {
+		
+		this.name = name;
+		this.attack = attack;
+		this.health = health;
+		
+		
+	}
+	
+	
+}
